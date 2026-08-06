d8118487aa6535ca3585028501e58e50bac07f01
林逸 <dbghpixian@outlook.com>
2026-08-06 20:44:11 +0800

    21.5 修复立即同步冲突函数引用

--- src/js/app.js
+++ src/js/app.js
@@ -43,6 +43,7 @@ import {
 import { exportSyncBundle, importSyncBundle } from "./sync_bundle.js";
 import {
   flushOutbox,
+  getPendingConflict,
   getSyncStatus,
   initSyncEngine,
   onSyncEnabledChanged,
